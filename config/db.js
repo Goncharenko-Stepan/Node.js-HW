b3bb7f6783f3cc98d34d633233c27d0d26058181
Goncharenko-Stepan <bebrou1234@gmail.com>
2024-11-05 17:40:44 +0100

    HW7

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import config from "./config.json" assert { type: "json" };
+
+const dbConfig = config.development;
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+  }
+);
+
+export default sequelize;
---
+++ migrations/20241105152719-create-apps-table.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Apps", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      size: { type: Sequelize.INTEGER, allowNull: false },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Apps");
+  },
+};
+
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.createTable('Apps', {
+//       id: {
+//         type: Sequelize.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false
+//       },
+//       name: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//       },
+//       size: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false
+//       }
+//     });
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.dropTable('Apps');
+//   }
+// };
